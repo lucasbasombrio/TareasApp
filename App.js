7b7c88b6173bd1e238a7d4ceb358822d1b387a40
lucasbasombrio <basombrio.lucas5@gmail.com>
2024-05-30 23:39:16 -0300

    Agregamos plantilla login

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import RegisterLoginScreen from './screens/RegisterLoginScreen';
+import { HomeScreen } from './HomeScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Visualizar Cambios</Text>
-      <StatusBar style="auto" />
-    </View>
+<NavigationContainer>
+  <Stack.Navigator initialRouteName='RegisterLogin'>
+    <Stack.Screen name="RegisterLogin" component={RegisterLoginScreen}/>
+    <Stack.Screen name="Home" component={HomeScreen}/>
+  </Stack.Navigator>
+  </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
